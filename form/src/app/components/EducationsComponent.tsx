50eb18158ddf34190b5e995b5430d4a455e24b60
KeyurPat006 <keyurcpatel006@gmial.com>
2026-02-28 14:43:44 -0500

    Add education val and picture size val

--- form/src/app/components/EducationsComponent.tsx
+++ form/src/app/components/EducationsComponent.tsx
@@ -3,12 +3,18 @@ import { Dispatch, SetStateAction } from "react"
 import type { Education, Form } from "../interfaces";
 import { createEmptyEducation } from "../interfaces";
 
+import { setEducationName } from "../utils/onChanges"
 import { setEducationDegree } from "../utils/onChanges"
+import { setEducationGPA } from "../utils/onChanges"
+import { setEducationScale } from "../utils/onChanges"
+import { setEducationStartDate } from "../utils/onChanges"
+import { setEducationEndDate } from "../utils/onChanges"
+import { setEducationDescription } from "../utils/onChanges"
 
 import formStyles from "../styles/form.module.css"
 
 type EducationsComponentProps = {
-    educations: Education[];
+    educations:Education[];
     setFormData: Dispatch<SetStateAction<Form>>;
 };
 
@@ -28,13 +34,7 @@ const EducationsComponent = ({
                             name={`educations[${index}].name`}
                             placeholder="School Name"
                             value={education.name}
-                            onChange={(e) =>
-                                setFormData((prev) => {
-                                    const updated = [...prev.educations];
-                                    updated[index].name = e.target.value;
-                                    return { ...prev, educations: updated };
-                                })
-                            }
+                            onChange={(e) => setEducationName(e.target.value, index, setFormData)}
                         />
                         <input
                             type="text"
@@ -48,72 +48,47 @@ const EducationsComponent = ({
                             name={`educations[${index}].gpa.gpa`}
                             placeholder="GPA"
                             value={education.gpa.gpa}
-                            onChange={(e) =>
-                                setFormData((prev) => {
-                                    const updated = [...prev.educations];
-                                    updated[index].gpa.gpa = Number(e.target.value);
-                                    return { ...prev, educations: updated };
-                                })
-                            }
                             step="0.01"
                             min="0"
                             max={education.gpa.scale || 4}
+                            onChange={(e) => setEducationGPA(e.target.value, index, setFormData)}
                         />
                         <input
                             type="number"
                             name={`educations[${index}].gpa.scale`}
                             placeholder="GPA Scale"
                             value={education.gpa.scale || ""}
-                            onChange={(e) =>
-                                setFormData((prev) => {
-                                    const updated = [...prev.educations];
-                                    updated[index].gpa.scale = Number(e.target.value);
-                                    return { ...prev, educations: updated };
-                                })  
-                            }
+                            onChange={(e) => setEducationScale(e.target.value, index, setFormData)}
                             step="0.01"
                             min="0"
                         />
+
                         <div className="dates">
                             <input
                                 type="month"
                                 name={`educations[${index}].dates.start`}
                                 placeholder="Start Date"
                                 value={education.dates.start}
-                                onChange={(e) => {
-                                    setFormData((prev) => {
-                                        const updated = [...prev.educations];
-                                        updated[index].dates.start = e.target.value;
-                                        return { ...prev, educations: updated };
-                                    })  
-                                }}
+                                onChange={(e) => setEducationStartDate(e.target.value, index, setFormData)}
                             />
+
                             <input
                                 type="month"
                                 name={`educations[${index}].dates.end`}
                                 placeholder="End Date"
                                 value={education.dates.end}
-                                onChange={(e) => {
-                                    setFormData((prev) => {
-                                        const updated = [...prev.educations];
-                                        updated[index].dates.end = e.target.value;
-                                        return { ...prev, educations: updated };
-                                    })  
-                                }}
+                                onChange={(e) => setEducationEndDate(e.target.value, index, setFormData)}
                             />
+
+
                             <textarea
                                 name={`educations[${index}].description`}
                                 placeholder="Description"
                                 value={education.description}
-                                onChange={(e) =>
-                                    setFormData((prev) => {
-                                        const updated = [...prev.educations];
-                                        updated[index].description = e.target.value;
-                                        return { ...prev, educations: updated };
-                                    })
-                                }
+                                onChange={(e) => setEducationDescription(e.target.value, index, setFormData)}
                                 rows={3}
                             />
+
                         </div>
                         <button
                             onClick={(e) => {
--- form/src/app/utils/onChanges.ts
+++ form/src/app/utils/onChanges.ts
@@ -20,16 +20,124 @@ export const setBio = (
                     
 
 // Education
+
+        //School Name
+export const setEducationName = (
+    newValue: string,
+    index: number,
+    setFormData: Dispatch<SetStateAction<Form>>
+) =>{
+    if(validations.validateEducationName(newValue)) {
+        setFormData(
+            (prev) => {
+            const updated = [...prev.educations];
+            updated[index].name = newValue;
+            return { ...prev, educations: updated };
+        }) 
+    }
+    
+
+}
+
+        //Degree
 export const setEducationDegree = (
     newValue: string,
     index: number,
     setFormData: Dispatch<SetStateAction<Form>>
 ) => {
+    if(validations.validateEducationDegree(newValue)) {
     setFormData((prev) => {
         const updated = [...prev.educations];
         updated[index].degree = newValue;
         return { ...prev, educations: updated };
-    })     
+    })
+}     
+}
+
+
+
+        //GPA
+export const setEducationGPA = (
+    newValue: string,
+    index: number,
+    setFormData: Dispatch<SetStateAction<Form>>
+) => {
+
+    const gpaNum = Number(newValue);
+    if(validations.validateEducationGPA(newValue)){
+    setFormData((prev) => {
+        const updated = [...prev.educations];
+        updated[index].gpa.gpa = gpaNum;
+        return { ...prev, educations: updated };
+    })
+}
+}
+
+        //GPA Scale
+export const setEducationScale = (
+    newValue: string,
+    index: number,
+    setFormData: Dispatch<SetStateAction<Form>>
+) => {
+    const scaleNum = Number(newValue);
+    if(validations.validateEducationScale(newValue)){
+    setFormData((prev) => {
+        const updated = [...prev.educations];
+        updated[index].gpa.scale = scaleNum;
+        return { ...prev, educations: updated };
+    })  
+}
+}
+
+    //Start Date 
+    export const setEducationStartDate = (
+        newValue: string,
+        index: number,
+        setFormData: Dispatch<SetStateAction<Form>>
+      ) => {
+        setFormData((prev) => {
+          const updated = [...prev.educations];
+          const currentEnd = updated[index].dates.end;
+      
+          
+          if (!validations.validateEducationStartDate(newValue, currentEnd)) return prev;
+      
+          updated[index].dates.start = newValue;
+          return { ...prev, educations: updated };
+        });
+      };
+
+
+    //End Date
+export const setEducationEndDate = (
+  newValue: string,
+  index: number,
+  setFormData: Dispatch<SetStateAction<Form>>
+) => {
+  setFormData((prev) => {
+    const updated = [...prev.educations];
+    const currentStart = updated[index].dates.start;
+
+    if (!validations.validateEducationEndDate(newValue, currentStart)) return prev;
+
+    updated[index].dates.end = newValue;
+    return { ...prev, educations: updated };
+  });
+};
+
+    //Description
+export const setEducationDescription = (
+    newValue: string, 
+    index: number,
+    setFormData: Dispatch <SetStateAction<Form>>
+) => {
+    if(validations.validateEducationDescription(newValue)){
+    setFormData((prev) => {
+        const updated = [...prev.educations];
+        updated[index].description = newValue;
+        return { ...prev, educations: updated };
+    })
+}
 }
 
 // Graduation
@@ -46,6 +154,16 @@ export const setEducationDegree = (
 
 // Picture
 
+export const setPicture = (
+    newValue: File,
+    setFormData: Dispatch<SetStateAction<Form>>
+) => {
+    if(validations.validatePicture(newValue)) {
+        setFormData(
+            (prev) => ({ ...prev, picture: newValue })
+        )
+    }
+}
 
 // Projects
 
