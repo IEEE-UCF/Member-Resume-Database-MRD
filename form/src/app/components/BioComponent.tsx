aad3a3f94595e956392f02fd6d822e5c943c2a83
Tal Avital <108590031+TAvital04@users.noreply.github.com>
2026-03-19 22:25:28 -0500

    display and error array

--- form/src/app/components/BioComponent.tsx
+++ form/src/app/components/BioComponent.tsx
@@ -1,6 +1,8 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import type { Form } from "../interfaces";
+
 import { setBio } from "../utils/onChanges"
+import { validateBio } from "../utils/validations"
 
 type BioComponentProps = {
     bio: string;
@@ -11,15 +13,38 @@ const BioComponent = ({
     bio, 
     setFormData, 
 }: BioComponentProps) => {
+    const [errors, setErrors] = useState<string[]>([])
+
     return (
         <>
             <h3>Bio</h3>
+
+            {
+                errors.length > 0 && 
+                (<>
+                    <p>BIO IS NOT VALID BECAUSE:</p>
+
+                    {errors.map((element, index) => {
+                        return <p key = {index}>{element}</p>
+                    })}
+                </>)
+            }
+
             <textarea
                 name="bio"
                 value={bio}
                 onChange={(e) => setBio(e.target.value, setFormData)}
                 rows={5}
             />
+
+            <button
+                onClick = {(e) => {
+                    e.preventDefault()
+                    setErrors(validateBio(bio))
+                }}
+            >
+                Submit
+            </button>
         </>
     );
 };
