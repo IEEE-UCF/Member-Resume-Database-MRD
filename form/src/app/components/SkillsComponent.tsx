57352a487e5c22721266518843e7a472c0d3c4be
Tal Avital <108590031+TAvital04@users.noreply.github.com>
2026-05-09 10:02:17 -0400

    commit skill again for some reason

--- form/src/app/components/SkillsComponent.tsx
+++ form/src/app/components/SkillsComponent.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { type Form } from "../interfaces";
 
 import SkillComponent from "./SkillComponent";
